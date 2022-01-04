f34b377a13d0f071283e4e3d22660c294149d571
JinalAhir1204 <93920331+JinalAhir1204@users.noreply.github.com>
2022-01-04 20:58:43 +0530

    Update MainWindow.xaml.cs
    Updated the code file for syntax checking. The program now contains logic to derive code blocks such as If...EndIf, While...EndWhile, Var XYZ=SomeValue and Method...EndMethod

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace WpfApplication1
     public partial class MainWindow : Window
     {
         CommandParser cmdParser = new CommandParser();
+        List<string> programBlocks = new List<string>();
+        public Dictionary<string, string> variables_values = new Dictionary<string, string>();
         public MainWindow()
         {
             InitializeComponent();
@@ -53,19 +55,8 @@ namespace WpfApplication1
                 txtCommand.Text = "";
                 e.Handled = true;
             }
+        }
 
-        }
-       
-        /// <summary>
-        /// This Button Save the Command
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btnSave_Click(object sender, RoutedEventArgs e)
-        {
-            String filepath = ".\\" + "MyProgram_" + DateTime.Now.Ticks.ToString() + ".txt";
-            File.WriteAllText(filepath, txtProgram.Text);
-        }
         /// <summary>
         /// This Button redirect to the saved commad file and select the pervious saved file and 
         /// load the previous command 
@@ -78,6 +69,18 @@ namespace WpfApplication1
             if (openFileDialog.ShowDialog() == true)
                 txtProgram.Text = File.ReadAllText(openFileDialog.FileName);
         }
+
+        /// <summary>
+        /// This Button Save the Command
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            String filepath = ".\\" + "MyProgram_" + DateTime.Now.Ticks.ToString() + ".txt";
+            File.WriteAllText(filepath, txtProgram.Text);
+        }
+
         /// <summary>
         /// This is single line command
         /// </summary>
@@ -107,6 +110,7 @@ namespace WpfApplication1
                 }
             }
         }
+
         /// <summary>
         /// This button run the multiline commad
         /// </summary>
@@ -114,13 +118,171 @@ namespace WpfApplication1
         /// <param name="e"></param>
         private void btnRun_Click(object sender, RoutedEventArgs e)
         {
-            foreach (string command in txtProgram.Text.Split('\n'))
+            if (txtProgram.Text.Contains("While ") || //this program contains code which cannot be simply executed line by line
+                txtProgram.Text.Contains("EndWhile\n") || 
+                txtProgram.Text.Contains("If ") ||
+                txtProgram.Text.Contains("EndIf\n") ||
+                txtProgram.Text.Contains("Method ") ||
+                txtProgram.Text.Contains("EndMethod\n") || 
+                txtProgram.Text.Contains("Var ")
+            )
             {
-                cmdParser.setCommandParser(command.Trim());
-                cmdParser.executeCommand();
-                txtOutput.Text = txtOutput.Text + "\n" + command.Trim();
+                //from the program we first divide the program into appropriate code blocks... if endif, while endwhile, method endmethod
+                //moveto
+                //drawto
+                //clear
+                //reset
+                //rectangle
+                //circle
+                //triangle
+                //pen
+                //fill
+                //Var 
+                string[] lines = txtProgram.Text.Split('\n');
+                
+                for(int i = 0; i< lines.Length;i++)
+                {
+                    string codeblock = "";//we declare this here as everyline can be a code block
+
+                    while (lines[i].Contains("  "))//we ensure that command only contain a single space as separator
+                    {
+                        lines[i] = lines[i].Replace("  ", " ");
+                    }
+
+                    while (lines[i].Contains("\t"))//we ensure that command only contain a single space as separator
+                    {
+                        lines[i] = lines[i].Replace("\t", " ");
+                    }
+                    
+
+                    if (lines[i].StartsWith("While "))//while condition
+                    {
+                        //code block finishes at EndWhile
+                        for (int j = i; j < lines.Length; j++, i++)
+                        {
+                            codeblock = codeblock + lines[i];
+                            if (lines[i].Trim().Equals("EndWhile"))
+                            {
+                                programBlocks.Add(codeblock);
+                                break;
+                            }
+                        }
+                        continue;
+                    }
+                    else if (lines[i].StartsWith("If "))//if condition
+                    {
+                        //code block finishes at EndWhile
+                        for (int j = i; j < lines.Length; j++, i++)
+                        {
+                            codeblock = codeblock + lines[i];
+                            if (lines[i].Trim().Equals("EndIf"))
+                            {
+                                programBlocks.Add(codeblock);
+                                break;
+                            }
+                        }
+                        continue;
+                    }
+                    else if (lines[i].StartsWith("Method "))//method name
+                    {
+                        //code block finishes at EndWhile
+                        for (int j = i; j < lines.Length; j++, i++)
+                        {
+                            codeblock = codeblock + lines[i];
+                            if (lines[i].Trim().Equals("EndMethod"))
+                            {
+                                programBlocks.Add(codeblock);
+                                break;
+                            }
+                        }
+                        continue;
+                    }
+                    else if (lines[i].StartsWith("Var ") ||
+                                lines[i].StartsWith("moveto ") ||
+                                lines[i].StartsWith("drawto ") ||
+                                lines[i].StartsWith("clear") ||
+                                lines[i].StartsWith("reset") ||
+                                lines[i].StartsWith("rectangle ") ||
+                                lines[i].StartsWith("circle ") ||
+                                lines[i].StartsWith("triangle ") ||
+                                lines[i].StartsWith("pen ") ||
+                                lines[i].StartsWith("fill ")                         
+                        )
+                    {
+                        //code block finishes at end of line
+                        string command = lines[i].Trim();
+                        programBlocks.Add(command);
+
+                        //we also initialize the variables
+                        if (command.StartsWith("Var "))
+                        {
+                            command = command.Replace("Var ", "Var$");
+                            string[] commands = command.Split('$');//$ is set as separator to distinguish it from space as a separator
+                            if (commands[0].Contains("Var") && (commands.Length == 2))
+                            {
+                                string[] varVal = commands[1].Split('=');
+                                string varName = varVal[0].Trim();
+                                string varValue = varVal[1].Trim();
+                                try
+                                {
+                                    variables_values.Add(varName, varValue);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                            }
+                            
+                        }
+                        continue;
+                    }
+                }
+                //we initialize all the variable values in a dictionary which are not in any of the above code blocks
+                //foreach (string command in txtProgram.Text.Split('\n'))//split with newline ensures that conditions like "abcVar" does not occur
+                                                                        //it always will be like abc \n Var
+                //{
+                //    if (command.Trim().Contains("Var "))
+                //    {
+                //        cmdParser.setCommandParser(command.Trim());
+                //    }
+                //}
+
             }
+            else{//this program is a script and can be executed line by line. It only contains the following
+                //moveto
+                //drawto
+                //clear
+                //reset
+                //rectangle
+                //circle
+                //triangle
+                //pen
+                //fill
+                foreach (string command in txtProgram.Text.Split('\n'))
+                {
+                    cmdParser.setCommandParser(command.Trim());
+                    cmdParser.executeCommand();
+                    txtOutput.Text = txtOutput.Text + "\n" + command.Trim();
+                }
+            }
+        }
+
+        private void btnCheckSyntax_Click(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = txtOutput.Text + "Program Blocks" + "\n";
+            foreach(string obj in programBlocks.ToArray())
+            {
+                txtOutput.Text = txtOutput.Text + "---------------------" + "\n";
+                txtOutput.Text = txtOutput.Text + "\n" + obj;
+            }
+            txtOutput.Text = txtOutput.Text + "Variables" + "\n";
+            foreach (KeyValuePair <string,string> obj in variables_values.ToArray())
+            {
+                txtOutput.Text = txtOutput.Text + "\n" + obj.Key + ": " + obj.Value;
+            }
+            
         }
     }
    
 }
+
